2f3fe222dda747ffe0235c4625a39b36fa37bc67
k.koide <k.koide@aist.go.jp>
2026-01-04 16:25:51 +0900

    cmake options

--- debian/01_build_debs.sh
+++ debian/01_build_debs.sh
@@ -9,11 +9,11 @@ cd ..
 dch -v $1-1ppa1~jammy1 $2 -D jammy
 git add debian/changelog
 git commit -m "update debian/changelog for jammy"
-gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules --git-force-create
+gbp buildpackage -S -sa --git-upstream-tag='4.3a0' --git-submodules --git-force-create
 
 dch -v $1-1ppa1~noble1 $2 -D noble
 git add debian/changelog debian/files
 git commit -m "update debian/changelog for noble"
-gbp buildpackage -S -sa --git-upstream-tag='v%(version)s' --git-submodules
+gbp buildpackage -S -sa --git-upstream-tag='4.3a0' --git-submodules
 
 popd
