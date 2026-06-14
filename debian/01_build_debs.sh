ac165b6153dee8e2bec13df0a1280f1de1385256
k.koide <k.koide@aist.go.jp>
2026-06-14 19:32:30 +0900

    4.3a1

--- debian/01_build_debs.sh
+++ debian/01_build_debs.sh
@@ -2,18 +2,21 @@
 export DEBFULLNAME="Kenji Koide"
 export DEBEMAIL="k.koide@aist.go.jp"
 
+# ARG1 = 4.3.0-1
+# ARG2 = "4.3a1"
+
 pushd .
 
 cd ..
 
-dch -v $1-1ppa1~jammy1 $2 -D jammy
+dch -v "$1"ppa1~jammy1 $2 -D jammy
 git add debian/changelog
 git commit -m "update debian/changelog for jammy"
-gbp buildpackage -S -sa --git-upstream-tag='4.3a0' --git-submodules --git-force-create
+gbp buildpackage -S -sa --git-upstream-tag='4.3a1' --git-submodules --git-force-create
 
-dch -v $1-1ppa1~noble1 $2 -D noble
+dch -v "$1"ppa1~noble1 $2 -D noble
 git add debian/changelog debian/files
 git commit -m "update debian/changelog for noble"
-gbp buildpackage -S -sa --git-upstream-tag='4.3a0' --git-submodules
+gbp buildpackage -S -sa --git-upstream-tag='4.3a1' --git-submodules
 
 popd
