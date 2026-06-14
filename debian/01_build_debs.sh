9b1a9dc4e6d3ae7e792ff8705fa65c96a396f09b
k.koide <k.koide@aist.go.jp>
2026-06-14 19:37:01 +0900

    add resolute

--- debian/01_build_debs.sh
+++ debian/01_build_debs.sh
@@ -2,21 +2,26 @@
 export DEBFULLNAME="Kenji Koide"
 export DEBEMAIL="k.koide@aist.go.jp"
 
-# ARG1 = 4.3.0-1
+# ARG1 = 4.3.0-2
 # ARG2 = "4.3a1"
 
 pushd .
 
 cd ..
 
-dch -v "$1"ppa1~jammy1 $2 -D jammy
+dch -v $1-1ppa1~jammy1 $2 -D jammy
 git add debian/changelog
 git commit -m "update debian/changelog for jammy"
 gbp buildpackage -S -sa --git-upstream-tag='4.3a1' --git-submodules --git-force-create
 
-dch -v "$1"ppa1~noble1 $2 -D noble
+dch -v $1-1ppa1~noble1 $2 -D noble
 git add debian/changelog debian/files
 git commit -m "update debian/changelog for noble"
+gbp buildpackage -S -sa --git-upstream-tag='4.3a1' --git-submodules
+
+dch -v $1-1ppa1~resolute1 $2 -D resolute
+git add debian/changelog debian/files
+git commit -m "update debian/changelog for resolute"
 gbp buildpackage -S -sa --git-upstream-tag='4.3a1' --git-submodules
 
 popd
